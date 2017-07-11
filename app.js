e7d598e64280b9b08b78ac7d399e9fd9b1cf3131
Ankur Gupta <ankurgupta5555@gmail.com>
2017-07-11 02:24:44 +0000

    added settings

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require("express"),
     bodyParser = require("body-parser"),
     expressSanitizer = require("express-sanitizer"),
+    passport = require("passport"),
+    Strategy = require("passport-twitter").Strategy,
+    session = require("express-session"),
+    morgan = require("morgan"),
+    cookieParser = require("cookie-parser"),
     app = express();
 
 //App config
@@ -10,7 +15,33 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(expressSanitizer());
+app.use(morgan('combined'));
+app.use(cookieParser());
+app.use(session({
+    secret: 'Auth for representative tweets',
+    resave: true,
+    saveUninitialized: true
+}));
 
+passport.use(new Strategy({
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    callbackURL: "https://representative-tweets-ankurgupta67.c9users.io/login/twitter/return"
+}, function(token, tokenSecret, profile, cb) {
+    return cb(null, profile);
+}));
+
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+    cb(null, obj);
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.get("/", function(req, res) {
     res.render("index");
